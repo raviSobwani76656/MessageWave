f1c2149273bf90f7dd474286334e99c41b952277
“Ravi <sobwaniravi@gmail.com>
2025-07-07 15:32:59 +0530

    Completed the message API

--- BackEnd/controllers/MessagesController.js
+++ BackEnd/controllers/MessagesController.js
@@ -71,4 +71,78 @@ const getMessage = async (req, res) => {
   }
 };
 
-module.exports = { sendMessage, getMessage };
+const deleteMessage = async (req, res) => {
+  try {
+    const { id } = req.body;
+
+    if (!id) {
+      console.log("The message you are looking to delete does not exist");
+      return res.status(404).json({
+        status: false,
+        message: "The message you are looking to delete does not exist",
+      });
+    }
+
+    const deletedMessage = await Messages.destroy({ where: { id } });
+
+    if (deletedMessage === 0) {
+      return res.status(404).json({
+        status: false,
+        message: "Message not found or already deleted",
+      });
+    }
+
+    return res
+      .status(200)
+      .json({ status: true, message: "Message deletion sucessfull" });
+  } catch (err) {
+    console.log("Error Occured While Deleting the Message", err);
+    return res.status(500).json({
+      status: false,
+      message: "Error Occured while deleting the Message",
+    });
+  }
+};
+
+const updateMessage = async (req, res) => {
+  try {
+    const { id, content } = req.body;
+
+    if (!id || !content) {
+      console.log("The message you are looking to update does not exist");
+      return res.status(404).json({
+        status: false,
+        message: "The message you looking to update does not exist",
+      });
+    }
+
+    const [updatedMessage] = await Messages.update(
+      { content },
+      { where: { id } }
+    );
+
+    if (!updatedMessage) {
+      console.log(
+        "The message to be update is already upto date or did not found"
+      );
+      return res.status(400).json({
+        status: false,
+        message:
+          "The message to be update is already upto date or did not found",
+      });
+    }
+
+    res.status(200).json({
+      status: true,
+      message: "The Message have been updated succesfully",
+    });
+  } catch (err) {
+    console.log("Error Occured while updating the Message", err);
+    res.status(500).json({
+      status: false,
+      message: "Error Occured while updating the message",
+    });
+  }
+};
+
+module.exports = { sendMessage, getMessage, deleteMessage, updateMessage };
--- BackEnd/routes/MessageRoute.js
+++ BackEnd/routes/MessageRoute.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const {
   sendMessage,
+  updateMessage,
+  deleteMessage,
   getMessage,
 } = require("../controllers/MessagesController");
 const router = express.Router();
 
 router.post("/sendMessage", sendMessage);
 router.get("/getMessage", getMessage);
+router.delete("/deleteMessage", deleteMessage);
+router.put("/updateMessage", updateMessage);
 
 module.exports = router;
